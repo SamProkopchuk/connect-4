01aaea076251588f09093f5b5bee1825977fd796
SamProkopchuk <ssprokopchuk@gmail.com>
2021-01-05 21:14:12 -0800

    Update test file

--- tests/test_cli_player.py
+++ tests/test_cli_player.py
@@ -1,10 +1,9 @@
 from connect_4.game_logic import Game, CLIPlayer
+from connect_4.ai import MiniMaxAI
 
 def main():
-    player_1 = CLIPlayer(1)
-    player_2 = CLIPlayer(2)
-    game = Game(player_1, player_2)
-    game.play()
+    game = Game(MiniMaxAI, CLIPlayer)
+    game.play(verbose=True)
 
 if __name__ == '__main__':
     main()
