c1558c71e94895ec1836c76e62b7f327bc01b56e
SamProkopchuk <ssprokopchuk@gmail.com>
2021-01-12 22:54:43 -0800

    Add requirements.txt & setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='connect-4',
+    version='1.0',
+    packages=['connect_4'],
+    url='',
+    license='',
+    author='SamProkopchuk',
+    author_email='ssprokopchuk@gmail.com',
+    description='An environment to test various Connect-4 AIs',
+    install_requires=['numpy>=1.18.5']
+)
