705d5ceea2abc8031a6cc9c33ecea167f45a8431
SamProkopchuk <ssprokopchuk@gmail.com>
2021-01-12 23:19:56 -0800

    Add setup license & python version

--- setup.py
+++ setup.py
@@ -9,5 +9,11 @@ setup(
     author='SamProkopchuk',
     author_email='ssprokopchuk@gmail.com',
     description='An environment to test various Connect-4 AIs',
-    install_requires=['numpy>=1.18.5']
+    install_requires=['numpy>=1.18.5'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
 )
